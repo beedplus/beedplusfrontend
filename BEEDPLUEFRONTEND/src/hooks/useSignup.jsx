287e5f08d798735acd2dd87bfeeb701b45dbc310
juwonoladele <juwonoladele772@gmail.com>
2024-01-31 04:07:08 +0100

    signup authentication

--- BEEDPLUEFRONTEND/src/hooks/useSignup.jsx
+++ BEEDPLUEFRONTEND/src/hooks/useSignup.jsx
@@ -4,7 +4,6 @@ import { redirect, useNavigate } from "react-router";
 
 export const useSignUp = () => {
   const [error, setError] = useState(null);
-  const [message, setMessage] = useState(null);
   const [ispending, setIspending] = useState(false);
   const [iscancelled, setisCancelled] = useState(false);
   const setAuth = usebackendStore((state) => state.setAuth);
@@ -12,13 +11,17 @@ export const useSignUp = () => {
 
   const apiUrl = "https://singularly-picked-grub.ngrok-free.app/auth/";
 
-
-  const signup = async (email, password, firstName, lastName) => {
+  const signup = async (firstName, lastName, email, password) => {
     setError(null);
     setIspending(true);
 
     try {
-      const postData = { email, password, firstName, lastName };
+      const postData = {
+        email,
+        password,
+        firstname: firstName,
+        lastname: lastName,
+      };
 
       const res = await fetch(`${apiUrl}/signup`, {
         method: "POST",
@@ -31,16 +34,16 @@ export const useSignUp = () => {
 
       const data = await res.json();
 
-      if (data.message) {
-        setError(data.message);
-        // setIspending(false);
-        setTimeout(() => {
-          setError(null);
-        }, 8000);
-      } else {
-            console.log(data)
-      }
-
+      //   if (data.message) {
+      //     setError(data.message);
+      //     // setIspending(false);
+      //     setTimeout(() => {
+      //       setError(null);
+      //     }, 8000);
+      //   } else {
+      //     console.log(data);
+      //   }
+      console.log(data);
       setIspending(false);
     } catch {
       if (!iscancelled) {
@@ -51,9 +54,7 @@ export const useSignUp = () => {
     }
   };
 
- 
-
   useEffect(() => () => setisCancelled(true), []);
 
-  return { signup, error, ispending, message };
+  return { signup, error, ispending };
 };
