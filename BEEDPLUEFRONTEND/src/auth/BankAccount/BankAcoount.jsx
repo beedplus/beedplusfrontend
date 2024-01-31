58e4b26f7bf5bc060c0de97bf0d7fe94155f0bd8
Oyiga victor <victoroyiga01@gmail.com>
2024-01-31 04:18:37 +0100

    commit

---
+++ BEEDPLUEFRONTEND/src/auth/BankAccount/BankAcoount.jsx
@@ -0,0 +1,18 @@
+import "./BankAcoount.css"
+
+export default function BankAcoount() {
+  return (
+    <div className="BankAcoount">
+        <div className="BankAcoount-form">
+            <h3>
+               Add your correct bank account
+               details to withdraw your earnings
+            </h3>
+            <div className="BankAcoount-input_div">
+                 <input type="text" />
+                 <input type="number"/>
+            </div>
+        </div>
+    </div>
+  )
+}
