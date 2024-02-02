3136b6661b8c48f1f10b70bbe056715616ab6d60
juwonoladele <juwonoladele772@gmail.com>
2024-02-02 11:28:22 +0100

    git commit

--- BEEDPLUEFRONTEND/src/components/ChallengeLinks/ChallengeLinks.jsx
+++ BEEDPLUEFRONTEND/src/components/ChallengeLinks/ChallengeLinks.jsx
@@ -36,7 +36,11 @@ const ChallengeLinks = () => {
     setDivCount(divCount + 1);
   };*/
 
-  let { error, isPending, documents } = useGetSingleCampaign(id);
+  let {
+    error: err,
+    isPending: ispending,
+    documents,
+  } = useGetSingleCampaign(id);
 
   // let {error, isPending, documents} = useGetAllCampaign()
 
@@ -44,11 +48,8 @@ const ChallengeLinks = () => {
     console.log(documents.data);
   }, [documents]);
 
-    const videoRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [showButton, setShowButton] = useState(false);
-    const [link, setlink] = useState("")
-
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const togglePlayPause = () => {
     const video = videoRef.current;
@@ -60,16 +61,9 @@ const ChallengeLinks = () => {
       setIsPlaying(false);
     }
   };
-  const handleMouseEnter = () => {
-    setShowButton(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowButton(false);
-  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    submit(link1,link2,link3,link4,Link5)
+    submit(link1, link2, link3, link4, Link5);
   };
 
   return (
@@ -125,34 +119,29 @@ const ChallengeLinks = () => {
           </div>
         </div>
 
-      <div className='challenge-requirements'>
-        {activeTab ? (
-          <div className="instruction active">
-            <header>Requirements</header>
-            <ul>
-              <li>Pick a challenge task you are interested in</li>
-              <li>Create 5 videos with the song and post it</li>
-              <li>The more you create, the more you win!</li>
-            </ul>
-            <header>Descriptions</header>
-            <ul>
-              <li>Pick a challenge task you are interested in</li>
-              <li>Create 5 videos with the song and post it</li>
-              <li>The more you create, the more you win!</li>
-            </ul>
-            <header>Video post</header>
-           
-            <div
-             className="challenge-video-container"
-             onMouseEnter={handleMouseEnter}
-             onMouseLeave={handleMouseLeave}
-             onClick={() => setShowButton(!showButton)}
-             >
-                <video  ref={videoRef} 
-                className='challenge-video'
-                onClick={togglePlayPause}
-                >
-                    <source src={documents.data.demo_video} type="video/mp4"></source>
+        <div className="challenge-requirements">
+          {activeTab ? (
+            <div className="instruction active">
+              <header>Requirements</header>
+              <ul>
+                <li>Pick a challenge task you are interested in</li>
+                <li>Create 5 videos with the song and post it</li>
+                <li>The more you create, the more you win!</li>
+              </ul>
+              <header>Descriptions</header>
+              <ul>
+                <li>Pick a challenge task you are interested in</li>
+                <li>Create 5 videos with the song and post it</li>
+                <li>The more you create, the more you win!</li>
+              </ul>
+              <header>Video post</header>
+
+              <div className="challenge-video-container">
+                <video ref={videoRef} className="challenge-video">
+                  <source
+                    src={documents.data.demo_video}
+                    type="video/mp4"
+                  ></source>
                 </video>
 
                 <div className="challenge-video-overlay">
