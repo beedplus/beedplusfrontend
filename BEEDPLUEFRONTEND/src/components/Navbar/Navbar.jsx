f100831b5a4795212503bf6858adb66a8468fb26
Oyiga victor <victoroyiga01@gmail.com>
2024-02-06 14:44:30 +0100

    stall bank  submission

--- BEEDPLUEFRONTEND/src/components/Navbar/Navbar.jsx
+++ BEEDPLUEFRONTEND/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import "./Navbar.scss"
 import Beed from "../../assets/image1.svg"
 import { useState } from "react";
 import { usebackendStore } from "../../store/store";
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
   const accessToken = usebackendStore((state) => state.accessToken);
  
  if (accessToken) { return (<nav className="nav">
-                    <img src={Beed} alt="website logo" />
+                       <a href  = '/'><img src={Beed} alt="website logo" /></a>
                     <div>  
                       <button onclick="location.href='/signup'"><a href = "/auth/Signup">CAMPAIGNS</a></button>
                       <a href = "/auth/Signup">Login</a>
@@ -15,9 +16,8 @@ export default function Navbar() {
                   
                   </nav>)}
   else {return (
-   
                      <nav className="nav">
-                  <img src={Beed} alt="website logo" />
+                       <img src={Beed} alt="website logo" />
                   <div className="link-holder">
                     <button onclick="location.href='/signup'"className="nav-button"style={{width: 90.3, height: 28.21}} >Earn Now </button>
                     <a href = "/auth/Signup">Login</a>
