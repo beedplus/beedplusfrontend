56ac610ec1327c7066f5184178e04ab2cd0b3bd5
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-02-06 04:34:57 +0100

    still css changhes

--- BEEDPLUEFRONTEND/src/components/Navbar/Navbar.jsx
+++ BEEDPLUEFRONTEND/src/components/Navbar/Navbar.jsx
@@ -2,15 +2,27 @@ import "./Navbar.scss"
 import Beed from "../../assets/image1.svg"
 import { useState } from "react";
 import { usebackendStore } from "../../store/store";
+import { FaRegBellSlash } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
 
 export default function Navbar() {
   const accessToken = usebackendStore((state) => state.accessToken);
  
  if (accessToken) { return (<nav className="nav">
-                    <img src={Beed} alt="website logo" />
-                    <div>  
-                      <button onclick="location.href='/signup'"><a href = "/auth/Signup">CAMPAIGNS</a></button>
-                      <a href = "/auth/Signup">Login</a>
+                    <div>
+                        <img src={Beed} alt="website logo" />
+                    </div>
+                    <div className="signed-in-navbar-div">
+                      <button>
+                          <a href = "/auth/Signup">CAMPAIGNS</a>
+                      </button>
+                      <p className="singned-nav-user-icon">
+                          <a href = "/notification"><FaRegBellSlash className="navbar-notification"/></a>
+                      </p>
+                      <p className="navbar-profile-icon-div">
+                          <a href="/profile" > <FaRegUser className="navbar-profile-icon" /> </a>
+
+                      </p>
                     </div>
                   
                   </nav>)}
