29fb52013410903048cd42260c8c1a81e1cce156
K-Honsu <feranmia51@gmail.com>
2024-03-16 12:44:09 +0100

    fix:work on token and error for the admin request page

--- BEEDPLUEFRONTEND/src/hooks/useAdminLogin.jsx
+++ BEEDPLUEFRONTEND/src/hooks/useAdminLogin.jsx
@@ -1,58 +1,55 @@
 import { useState, useEffect } from "react";
 import { usebackendStore } from "../store/store";
 export const useAdminLogin = () => {
-    const [error, setError] = useState(null);
-    const [ispending, setIspending] = useState(false);
-    const [iscancelled, setisCancelled] = useState(false);
-    const setTempAuth = usebackendStore((state) => state.setTempAuth);
+  const [error, setError] = useState(null);
+  const [ispending, setIspending] = useState(false);
+  const [iscancelled, setisCancelled] = useState(false);
+  const setTempAuth = usebackendStore((state) => state.setTempAuth);
 
+  const apiUrl = "https://beedplus.onrender.com/admin";
 
+  const login = async (email, password) => {
+    setError(null);
+    setIspending(true);
 
-    const apiUrl = "https://beedplus.onrender.com/admin";
+    try {
+      const postData = {
+        email,
+        password,
+      };
 
-    const login = async (email, password) => {
-        setError(null);
-        setIspending(true);
+      const res = await fetch(`${apiUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Add any other headers if needed
+        },
+        body: JSON.stringify(postData),
+      });
+      console.log({ "res1": res });
 
-        try {
-            const postData = {
-                email,
-                password,
-            };
+      const data = await res.json();
+      console.log({ "1": data.token });
+      if (data.status === "success") {
+        setTempAuth(data.data._id, data.token);
+      }
+      if (data.status === "error") {
+        setError(data.message);
+      }
+      if (data.message) {
+        setError(data.message);
+      }
+      setIspending(false);
+    } catch {
+      if (!iscancelled) {
+        setError("Unknown Error occured");
+      }
 
-            const res = await fetch(`${apiUrl}/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    // Add any other headers if needed
-                },
-                body: JSON.stringify(postData),
-            });
+      setIspending(false);
+    }
+  };
 
-            const data = await res.json();
-            console.log(data)
-            if (data.status === "success") {
+  useEffect(() => () => setisCancelled(true), []);
 
-                setTempAuth(data.data._id, data.token);
-
-            }
-            if (data.status === "error") {
-                setError(data.message);
-            }
-            if(data.message){
-                setError(data.message)
-            }
-            setIspending(false);
-        } catch {
-            if (!iscancelled) {
-                setError("Unknown Error occured");
-            }
-
-            setIspending(false);
-        }
-    };
-
-    useEffect(() => () => setisCancelled(true), []);
-
-    return { login, error, ispending };
+  return { login, error, ispending };
 };
