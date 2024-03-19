eaf83d251d8bdc6e77bf236d05f3b547d84b6a89
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-03-19 17:49:12 +0100

    pushed

--- BEEDPLUEFRONTEND/src/components/SydneyHomeLogo/Sydney.jsx
+++ BEEDPLUEFRONTEND/src/components/SydneyHomeLogo/Sydney.jsx
@@ -1,25 +1,41 @@
 import "../SydneyHomeLogo/sydney.scss"
-import money from "/Users/josiah/Documents/beedplus/beedplusfrontend/BEEDPLUEFRONTEND/src/assets/money.png"
-import music from "/Users/josiah/Documents/beedplus/beedplusfrontend/BEEDPLUEFRONTEND/src/assets/music.png"
-import reading from "/Users/josiah/Documents/beedplus/beedplusfrontend/BEEDPLUEFRONTEND/src/assets/reading.png"
+import money from "../../assets/pic-12-removebg-preview.png"
+import music from "../../assets/depositphotos_3376847-stock-photo-music.jpg"
+import reading from "/Users/josiah/Documents/beedplus/beedplusfrontend/BEEDPLUEFRONTEND/src/assets/reading2.jpg"
 import sydney from "/Users/josiah/Documents/beedplus/beedplusfrontend/BEEDPLUEFRONTEND/src/assets/WhatsApp Image 2024-02-07 at 12.46.00.jpeg"
 const Sydney = () =>{
     return(
         <section className="sydney-homepage-component">
             <div className="sydney-homepage-component-div">
                 <section className="sydney-homepage-top-images">
-                    <div>
+                    <div className="sydney-homepage-potrait-image">
                         <img alt="sydney" src={sydney}/>
                     </div>
-                    <div>
-                        <img alt="sydney" src={music}/>
+                    <div className="sydney-homepage-music-logo-section">
+                        <div className="shmls-top">
+                            <p>
+
+                            </p>
+                        </div>
+                        <div className="shmls-bottom">
+                            <img alt="sydney" src={music}/>
+                        </div>
+
                     </div>
                 </section>
                 <section className="sydney-homepage-bottom-images">
-                    <div>
-                        <img alt="sydney" src={money}/>
+                    <div className="sydney-homepage-money-logos">
+                        <div className="shml-red">
+                            <p>
+
+                            </p>
+                        </div>
+                        <div className="homepage-dollar-image">
+                            <img alt="sydney" src={money}/>
+                        </div>
+
                     </div>
-                    <div>
+                    <div className="sydney-homepage-reading-logo">
                         <img alt="sydney" src={reading}/>
                     </div>
                 </section>
