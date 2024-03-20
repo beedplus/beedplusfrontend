069ddba3d95d6693f0f51a3aef757e6c9c7a60bc
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-03-20 18:38:33 +0100

    commit

---
+++ BEEDPLUEFRONTEND/src/components/windowclosepage/Windowclosepage.jsx
@@ -0,0 +1,13 @@
+import {useEffect} from "react";
+
+const Windowclosepage = () => {
+    useEffect(() => {
+        window.close();
+    }, []);
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Windowclosepage()
